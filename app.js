31902076c72ef97dcbd408977dd848d2ae875e80
Himanshu Raj <HRaj@ABS-CBN.COM>
2022-03-10 02:06:48 +0530

    change start script

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 3000;
 
-app.use("/", (req, res) => {
-  res.send("Hello World !");
+app.get("/", (req, res) => {
+  res.json({ Message: "Hello" });
 });
 
 app.listen(port, (err) => {
