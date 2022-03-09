233ba008da440a702d15a98fa74e7e66c672bfba
Himanshu Raj <HRaj@ABS-CBN.COM>
2022-03-10 01:54:36 +0530

    port changed

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+const port = process.env.port || 3000;
 
 app.use("/", (req, res) => {
   res.send("Hello World !");
 });
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
   if (!err) {
-    console.log("App listening on PORT 3000");
+    console.log("App listening on PORT : ", port);
   } else {
     console.log("Error in listening app");
   }
